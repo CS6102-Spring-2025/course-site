d5e0eb718ca2f0619bbd30af752fe68bafd87517
shoaibubaid <shoaibubaid@users.noreply.github.com>
2026-02-13 12:09:31 +0000

    deploy: d583447c77cd4c6c320cdc04d68b44bb50240557

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c313a138.js',
+                path_to_root + 'searchindex-f124ada6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
