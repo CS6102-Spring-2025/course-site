1118bca86ff06cd0bd59a0081be0c7a8d0f167de
belelaritra <belelaritra@users.noreply.github.com>
2026-04-01 07:43:32 +0000

    deploy: b44cbe3a1079699afee37427f999928a6465de31

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-75cb4d27.js',
+                path_to_root + 'searchindex-ea27aa83.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
